3f6a75d72a337d1e046ecf88c1efe6ae5149b0d9
FarruxDEV <farruxbeknozimov@gmail.com>
2023-03-23 11:10:01 +0500

    Add Status model

--- src/status/status.service.ts
+++ src/status/status.service.ts
@@ -1,36 +1,36 @@
-// import { Injectable, NotFoundException } from '@nestjs/common';
-// import { InjectModel } from '@nestjs/mongoose';
-// import { Model } from 'mongoose';
-// import { Status, StatusDocument } from './schemas/status.schema';
-// import { CreateStatusDto } from './dto/create-status.dto';
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Status, StatusDocument } from './schemas/status.schema';
+import { CreateStatusDto } from './dto/create-status.dto';
+import { UpdateStatusDto } from './dto/update-status.dto';
 
-// @Injectable()
-// export class StatusService {
-//   constructor(
-//     @InjectModel(Status.name)
-//     private statusModel: Model<StatusDocument>,
-//   ) {}
+@Injectable()
+export class StatusService {
+  constructor(
+    @InjectModel(Status.name) private statusModel: Model<StatusDocument>,
+  ) {}
 
-//   async create(createStatusDto: CreateStatusDto) {
-//     const res = new this.statusModel(createStatusDto);
-//     return res.save();
-//   }
+  async create(createStatusDto: CreateStatusDto) {
+    const res = new this.statusModel(createStatusDto);
+    return res.save();
+  }
 
-//   async findAll() {
-//     return this.statusModel.find().exec();
-//   }
+  async findAll() {
+    return this.statusModel.find().exec();
+  }
 
-//   async findOneById(id: string) {
-//     return this.statusModel.findById(id).exec();
-//   }
+  async findOneById(id: string) {
+    return this.statusModel.findById(id).exec();
+  }
 
-//   async update(id: string, updateCurrencyTypeDto: UpdateCurrencyTypeDto) {
-//     return this.statusModel
-//       .findByIdAndUpdate(id, updateCurrencyTypeDto, { new: true })
-//       .exec();
-//   }
+  async update(id: string, updateStatusDto: UpdateStatusDto) {
+    return this.statusModel
+      .findByIdAndUpdate(id, updateStatusDto, { new: true })
+      .exec();
+  }
 
-//   async remove(id: string) {
-//     return this.statusModel.findByIdAndDelete(id).exec();
-//   }
-// }
+  async remove(id: string) {
+    return this.statusModel.findByIdAndDelete(id).exec();
+  }
+}
